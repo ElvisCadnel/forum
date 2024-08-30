0bffea0bc20d3925421814718c7cd5937889e27d
Elvis Cadnel <elvisdjinato@gmail.com>
2024-08-30 10:35:48 +0100

    Résolution d'erreur

--- image.php
+++ image.php
@@ -14,10 +14,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
     $new_image_name = $file_basename . '_' . date("Ymd_His") . '.' . $file_extension;
 
-    $user_id=$_SESSION['id'];
     
     require('actions/database.php');
+    
+    if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) && !empty($_POST['mdp'])) {  
 
+        $checkIsUserAlredyExists = $base->prepare('SELECT pseudo FROM users WHERE pseudo = ?');
+    $checkIsUserAlredyExists->execute(array($users_name));
+   
+  
+   if ($checkIsUserAlredyExists->rowCount() == 0){
+    $user_id=$_SESSION['id'];
 
     $sql = "INSERT INTO image (user_id,libelle) VALUES ('$user_id','$new_image_name')";
 
@@ -34,14 +41,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
         $usersInfos = $getInfosUser->fetch();
        
         #authentifier l'utilisateur sur le site et récuperer ses donnees 
-        $_SESSION['image'] = $usersInfos['libelle'];
+        // $_SESSION['image'] = $usersInfos['libelle'];
   
-  
+    }
         if(!move_uploaded_file($_FILES["image"]["tmp_name"], $target_path)){
             header("Location:index.php?message=er");
         }
-      
-    }
     
+    }else{ 
+        $errormsg = "L'utilisateur existe déjà veuillez changer de pseudo";
+     }  
+    }else{ 
+       $errormsg = "Veuillez remplir tous les champs";
+    }  
 }
 }
